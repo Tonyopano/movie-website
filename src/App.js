a9afe86b291480bc03554c6a56fb84cb3712da18
Tonyopano <107053802+Tonyopano@users.noreply.github.com>
2023-12-01 17:39:32 +0300

    Design search button div

--- src/App.js
+++ src/App.js
@@ -17,8 +17,20 @@ const App = () => {
         searchMovies('Spiderman');
     }, []);
   return (
-    <h1>App</h1>
-
+    <div className="app">
+        <h1>MovieLand</h1>
+        <div className="search">
+            <input 
+                placeholder="Search for movies"
+                value=""
+                onChange={() => {}}
+            />
+            <img 
+                src={SearchIcon}
+                alt="search"
+            />
+        </div>
+    </div>
   );
 }
 
